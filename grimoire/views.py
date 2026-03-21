33312c859d000a19c87cfbc2581c9e50ffc35895
PedroH <phfigueiredolima@gmail.com>
2026-03-21 17:30:31 -0300

    ✨feat: implementa Update e Delete no CRUD
    - Implementa views editar_pessoa e deletar_pessoa.
    - Configura rotas dinâmicas com <int:id> no urls.py.
    - Adiciona formulário de edição e botões de ação na lista.

--- grimoire/views.py
+++ grimoire/views.py
@@ -21,4 +21,26 @@ def wizards(request):
 
 def cadastrar_pessoa(request):
     return render(request, 'cadastrar_pessoa.html')
+
+def editar_pessoa(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+
+    if request.method == "POST":
+        pessoa.nome = request.POST.get('nome')
+        pessoa.idade = request.POST.get('idade')
+        pessoa.email = request.POST.get('email')
+        pessoa.telefone = request.POST.get('telefone')
+        pessoa.save()
+        return redirect('wizards')
+    
+    return render(request, 'editar_pessoa.html', {'pessoa': pessoa})
+
+def deletar_pessoa(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.delete()
+    return redirect('wizards')
+
+
+
+
 # Create your views here.
