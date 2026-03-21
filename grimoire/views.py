11396ac891f21e92cec30b6b7cfb2acb0f61f219
PedroH <phfigueiredolima@gmail.com>
2026-03-21 16:54:02 -0300

    ✨feat: implementa cadastro de pessoas com CSRF
    - Cria template cadastrar_pessoa.html com formulário POST.
    - Adiciona lógica de salvamento e proteção CSRF.
    - Configura navegação entre a lista e o formulário de cadastro.

--- grimoire/views.py
+++ grimoire/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Pessoa
 
 def wizards(request):
+    if request.method == "POST":
+        Pessoa.objects.create(
+            nome=request.POST.get('nome'),
+            idade=request.POST.get('idade'),
+            email=request.POST.get('email'),
+            telefone=request.POST.get('telefone')
+        )
+        return redirect('wizards')
+    
     pessoas = Pessoa.objects.all().values()
-   
+
     contexto = {
         'meus_wizards': pessoas, 
     }
 
     return render(request, 'form.html', contexto)
 
+def cadastrar_pessoa(request):
+    return render(request, 'cadastrar_pessoa.html')
 # Create your views here.
